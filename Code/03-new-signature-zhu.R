9ea6dffd6f667a52ae3af7d0d6f9083541f1e1ef
sachsmc <sachsmc@gmail.com>
2016-02-19 17:00:28 -0500

    pca selection is a bust

--- Code/03-new-signature-zhu.R
+++ Code/03-new-signature-zhu.R
@@ -28,42 +28,52 @@ cldat <- gdat[, -psdex]
 
 fit.superpc <- function(dat0, cldat){
 
-  cors <- dat0 %>% group_by(gene) %>% summarize(cor = cor(zexpression, surv2.5))
+  cors <- dat0 %>% group_by(gene) %>%
+    do(data.frame(
+      t(summary(coxph(Surv(DSS.time, DSS.status == "Dead") ~ zexpression,
+                           data = .,
+                           eps = 1e-4))$coefficients[c(1,5)]))
+      )
+
   ## select number of genes
 
-  n.gen.cands <- 3:100
+  cors <- cors[unlist(cors$X2) < .005, ]
+  n.gen.cands <- 5:nrow(cors)
 
   p.ngenes <- sapply(n.gen.cands, function(n.genes){
 
-    sel.genes <- cors$gene[rank(-cors$cor) <= n.genes]
+    sel.genes <- cors$gene[rank(-abs(cors$X1)) <= n.genes]
+    coeffs <- cors$X1[rank(-abs(cors$X1)) <= n.genes]
 
-    pcas <- dat0 %>% select(ID, gene, zexpression) %>% filter(gene %in% sel.genes) %>% spread(gene, zexpression)
-    pcamat <- as.matrix(pcas[, -1])
+    pcas <- dat0 %>% select(ID, gene, zexpression) %>%
+      filter(gene %in% sel.genes) %>% spread(gene, zexpression)
+    pcamat <- as.matrix(pcas[, -1]) * matrix(rep(coeffs, nrow(pcas)), nrow = nrow(pcas), byrow = TRUE)
     rownames(pcamat) <- pcas$ID
 
-    pc.fit <- prcomp(pcamat, center = FALSE, scale. = FALSE)
-    pc.dat <- as.data.frame(pc.fit$x[, 1:3])
+    pc.fit <- prcomp(pcamat, center = TRUE, scale. = TRUE)
+    pc.dat <- as.data.frame(pc.fit$x[, 1:5])
     pc.dat$ID <- rownames(pc.dat)
 
     pdonk <- merge(cldat, pc.dat, by = "ID")
-    fit.cox <- coxph(Surv(DSS.time, DSS.status == "Dead") ~ PC1 + PC2 + PC3, data = pdonk)
-    summary(fit.cox)$logtest[3]
+    fit.cox <- coxph(Surv(DSS.time, DSS.status == "Dead") ~ PC1 + PC2 + PC3 + PC4 + PC5, data = pdonk)
+    summary(fit.cox)$concordance[1]
 
   })
 
-  n.gene.sel <- n.gen.cands[which(order(p.ngenes) == 1)]
-  sel.genes <- cors$gene[rank(-cors$cor) <= n.gene.sel]
+  n.gene.sel <- n.gen.cands[which(order(-abs(p.ngenes)) == 1)]
+  sel.genes <- cors$gene[rank(-abs(cors$X1)) <= n.gene.sel]
 
-  pcas <- dat0 %>% select(ID, gene, zexpression) %>% filter(gene %in% sel.genes) %>% spread(gene, zexpression)
+  pcas <- dat0 %>% select(ID, gene, zexpression) %>%
+    filter(gene %in% sel.genes) %>% spread(gene, zexpression)
   pcamat <- as.matrix(pcas[, -1])
   rownames(pcamat) <- pcas$ID
 
   pc.fit <- prcomp(pcamat, center = FALSE, scale. = FALSE)
-  pc.dat <- as.data.frame(pc.fit$x[, 1:3])
+  pc.dat <- as.data.frame(pc.fit$x[, 1:5])
   pc.dat$ID <- rownames(pc.dat)
 
   pdonk <- merge(cldat, pc.dat, by = "ID")
-  fit.cox <- coxph(Surv(DSS.time, DSS.status == "Dead") ~ PC1 + PC2 + PC3, data = pdonk)
+  fit.cox <- coxph(Surv(DSS.time, DSS.status == "Dead") ~ PC1 + PC2 + PC3 + PC4 + PC5, data = pdonk)
 
   lps <- predict(fit.cox, type = 'lp')
 
@@ -81,20 +91,37 @@ fit.superpc <- function(dat0, cldat){
 
 predict.superpc <- function(dat0, las.fit){
 
-  pcests <- predict(las.fit$pc.fit, newdata = dat0)[, 1:3]
+  pcests <- predict(las.fit$pc.fit, newdata = dat0)[, 1:5]
   dat0 <- cbind(dat0, pcests)
   coxests <- predict(las.fit$cox.fit, newdata = dat0, type = 'lp')
   list(lps = coxests, riskgrp = coxests > las.fit$cutoff)
 
+
+}
+
+eval.predict <- function(dat0, las.fit){
+
+  lpreds <- predict.superpc(dat0, las.fit)
+  sc <- survConcordance(Surv(dat0$DSS.time, dat0$DSS.status == "Dead") ~ lpreds$lps)
+
+  disc <- coxph(Surv(dat0$DSS.time, dat0$DSS.status == "Dead") ~ lpreds$riskgrp)$coefficients
+
+  c(calib = sc$concordance, discrim = disc)
+
 }
 
 
-## substitution estimate
+## substitution estimate using full dataset
 
-fit.obs <- fit.superpc(subset(ldat, Post.Surgical.Treatment == "OBS"), subset(cldat, Post.Surgical.Treatment == "OBS"))
+fit.all <- fit.superpc(subset(ldat, Post.Surgical.Treatment == "OBS"), cldat)
 #fit.obs <- fit.superpc(ldat, cldat)
+fitted <- predict.superpc(subset(gdat, Post.Surgical.Treatment == "OBS"), fit.all)
+eval.predict(subset(gdat, Post.Surgical.Treatment == "OBS"), fit.all)
 
-fitted <- predict.superpc(subset(gdat, Post.Surgical.Treatment == "OBS"), fit.obs)
+## partial resubstitution
 
-dat.ests <- data.frame(subset(gdat, Post.Surgical.Treatment == "OBS"), riskgrp = fitted$riskgrp)
-plot(survfit(Surv(DSS.time, DSS.status == "Dead") ~ riskgrp, data = dat.ests))
+
+
+
+
+
